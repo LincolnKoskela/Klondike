3905361e3c8f95af364689a0ad399d9699997dca
LincolnKoskela <Lincolnkoskelal@gmail.com>
2025-09-30 10:31:34 -0400

    Made the constructor for the Board

--- Board.java
+++ Board.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.EnumMap;
 
 /**
  * The Board class creates 7 tableau piles, 4 foundation piles, 1 stock and 1 waste. 
@@ -8,4 +9,22 @@ import java.util.ArrayList;
 
 public class Board {
     
+    private Stock stock;
+    private Waste waste;
+    private EnumMap<Card.Suit, Foundation> foundations;
+    private List<Tableau> tableaus;
+
+    public Board() {
+        this.stock = new Stock();
+        this.waste = new Waste();
+
+        this.foundations = new EnumMap<>(Card.Suit.class);
+
+        this.tableaus = new ArrayList<>();
+        for (int i = 0; i < 7; i++) {
+            this.tableaus.add(new Tableau());
+        }
+
+        
+    }
 }
