e64fc12e7b721afff495bc1db3b86d272e99c2ad
LincolnKoskela <Lincolnkoskelal@gmail.com>
2025-10-01 07:57:09 -0400

    Finished constructor, making enummaps for each suit and adding notes

--- Board.java
+++ Board.java
@@ -11,20 +11,33 @@ public class Board {
     
     private Stock stock;
     private Waste waste;
-    private EnumMap<Card.Suit, Foundation> foundations;
+    private EnumMap<Card.Suit, Foundation> foundations; // for each suit, associate 1 foundation
     private List<Tableau> tableaus;
 
+    /**
+     * Create a new Board object
+     */
     public Board() {
         this.stock = new Stock();
         this.waste = new Waste();
 
+        /*
+         * key is Card.suit 
+         * value is corresponding foundation pile object
+         * now there's 4 foundations, 1 for each suit
+         */
         this.foundations = new EnumMap<>(Card.Suit.class);
+        foundations.put(Card.Suit.CLUBS, new Foundation());
+        foundations.put(Card.Suit.DIAMONDS, new Foundation());
+        foundations.put(Card.Suit.HEARTS, new Foundation());
+        foundations.put(Card.Suit.SPADES, new Foundation());
 
         this.tableaus = new ArrayList<>();
         for (int i = 0; i < 7; i++) {
             this.tableaus.add(new Tableau());
         }
-
         
     }
+
+    // Initialization
 }
