ca58a606435e9842b1a09bdf65b5cf0fb8119a41
LincolnKoskela <Lincolnkoskelal@gmail.com>
2025-11-24 09:56:16 -0600

    JAVAFX project.

---
+++ src/main/java/GameApp.java
@@ -0,0 +1,9 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+
+public class GameApp {
+    
+}
