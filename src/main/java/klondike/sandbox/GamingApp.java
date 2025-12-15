a8366e563a49ddb8b0f0987b2c7445d7cf5eb8e5
LincolnKoskela <Lincolnkoskelal@gmail.com>
2025-12-15 14:57:47 -0600

    15DEC25 sandbox. Added mouse features, some styling. Centered Tabs.

--- src/main/java/klondike/sandbox/GamingApp.java
+++ src/main/java/klondike/sandbox/GamingApp.java
@@ -1,91 +1,74 @@
 package klondike.sandbox;
 import klondike.*;
 
+import java.util.Stack;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.*;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.stage.*;
 import javafx.scene.layout.*;
-import javafx.scene.control.ListView;
 
 public class GamingApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        BorderPane root = new BorderPane(); // top level root node 
+        BorderPane board = new BorderPane();
 
-        // ----------------sidebar menu custom----------------
-        ListView<String> sideMenu = new ListView<>();
-        sideMenu.getItems().addAll("Home", "Profile", "Settings", "Logout");
-        sideMenu.setPrefSize(140, 120);
-        sideMenu.setFixedCellSize(40); // each row height
+        StackPane root = new StackPane(board); // top level root node 
 
-        sideMenu.setStyle(
+        Button home = new Button("Home");
+        Button profile = new Button("Profile");
+        Button settings = new Button("Settings");
+        Button logout = new Button("Logout");
+
+        /**
+         * Style the buttons
+         * https://openjfx.io/javadoc/21/javafx.graphics/javafx/scene/doc-files/cssref.html
+         */
+        for (Button b : new Button[]{home, profile, settings, logout}) {
+            b.setMaxWidth(Double.MAX_VALUE);
+            b.setStyle(
+                "-fx-background-color: #262626;" + 
+                "-fx-text-fill: white;" + 
+                "-fx-text-font-size: 12px;"
+            );
+
+            b.setOnMouseEntered(e -> b.setStyle(
+                "-fx-background-color: #333333;" +
+                "-fx-text-fill: white;" +
+                "-fx-font-size: 12px;" +
+                "-fx-padding: 10 16;" +
+                "-fx-background-radius: 4;"
+            ));
+
+            b.setOnMouseExited(e -> b.setStyle(
+                "-fx-background-color: #262626;" +
+                "-fx-text-fill: white;" +
+                "-fx-font-size: 12px;" +
+                "-fx-padding: 10 16;" +
+                "-fx-background-radius: 4;"
+            ));
+                
+
+        }
+
+        // menu 
+        VBox menu = new VBox(10, home, profile, settings, logout);
+        menu.setStyle(
             "-fx-background-color: #262626;" + 
-            "-fx-control-inner-background: #262626;" + 
-            "-fx-border-color: #172a94b6;" + 
-            "-fx-border-width: 1;" + 
-            "-fx-padding: 0;"
+            "-fx-border-color: #555;" + 
+            "-fx-border-width: 1;"
         );
+        menu.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        menu.setPrefWidth(160);
+        root.getChildren().add(menu);
+        StackPane.setAlignment(menu, Pos.TOP_RIGHT);
 
-        /*
-        * This callback function is called whenever ListCell is created.
-        * Whenever listview needs a row, create a ListCell like this...
-        * Whenever that row needs to show a new item, call updateItem()
-        * and I(function) update it
-        */
-        sideMenu.setCellFactory(lv -> {
-            ListCell<String> cell = new ListCell<>() {
-                @Override
-                protected void updateItem(String item, boolean empty) {
-                    super.updateItem(item, empty);
-
-                    if (empty || item == null) {
-                        setText(null);
-                        setStyle("");
-                    } else {
-                        setText(item);
-                        setStyle(
-                            "-fx-text-fill: white;" +
-                            "-fx-font-size: 12px;" + 
-                            "-fx-padding: 4 10 4 10;" + 
-                            "-fx-background-color: transparent;"
-                        );
-                    }
-                }
-            };
-
-            cell.setOnMouseEntered(e -> {
-                if (!cell.isEmpty()) {
-                    cell.setStyle(
-                        "-fx-text-fill: white;" + 
-                        "-fx-font-size: 12px;" + 
-                        "-fx-padding: 4 10 4 10;" + 
-                        "-fx-background-color: #4f5255ff;"
-                    );
-                }
-            });
-
-            cell.setOnMouseExited(e -> {
-                if (!cell.isEmpty()) {
-                    cell.setStyle(
-                        "-fx-text-fill: white;" + 
-                        "-fx-font-size: 12px;" + 
-                        "-fx-padding: 4 10 4 10;" +
-                        "-fx-background-color: transparent;"
-                    );
-                }
-            });
-            return cell;
-        });
-
-        // ---------------------------------------------------------
-
-
-
+        // foundation
         HBox foundations = new HBox(40);
         foundations.getChildren().addAll(
             new CardSlot("F1"),
@@ -94,27 +77,34 @@ public class GamingApp extends Application {
             new CardSlot("F4")
         );
 
+        StackPane foundationWrapper = new StackPane(foundations);
+        foundationWrapper.setAlignment(Pos.TOP_LEFT);
+        root.getChildren().addAll(foundationWrapper);
+
         HBox tableaus = new HBox(40);
         tableaus.getChildren().addAll(
-            new Label("T1"),
-            new Label("T2"),
-            new Label("T3"),
-            new Label("T4"),
-            new Label("T5"),
-            new Label("T6"),
-            new Label("T7")
+            new CardSlot("T1"),
+            new CardSlot("T2"),
+            new CardSlot("T3"),
+            new CardSlot("T4"),
+            new CardSlot("T5"),
+            new CardSlot("T6"),
+            new CardSlot("T7")
         );
+        // Center the tabs by wrapping them
+        StackPane tableauWrapper = new StackPane(tableaus);
+        tableauWrapper.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        root.getChildren().add(tableauWrapper);
+        StackPane.setAlignment(tableauWrapper, Pos.CENTER);
 
         HBox stockWaste = new HBox(60);
         stockWaste.getChildren().addAll(
-            new Label("Stock"),
-            new Label("Waste")
+            new CardSlot("Stock"),
+            new CardSlot("Waste")
         );
 
-        root.setTop(foundations);
-        root.setCenter(tableaus);
-        root.setBottom(stockWaste);
-        root.setRight(sideMenu);
+        board.setBottom(stockWaste);
+        board.setTop(foundationWrapper);
 
         Scene scene = new Scene(root, 1200, 800, Color.BISQUE);
         root.setStyle("-fx-background-color: transparent;");
@@ -122,6 +112,7 @@ public class GamingApp extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
