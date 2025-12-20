9ed729ea1030bfc4d013fc9ea2c2e30905ef980b
LincolnKoskela <Lincolnkoskelal@gmail.com>
2025-12-20 12:26:19 -0600

    Added highlight around the selected Card in a pileView.

--- src/main/java/klondike/sandbox/PileView.java
+++ src/main/java/klondike/sandbox/PileView.java
@@ -14,8 +14,8 @@ public class PileView extends Pane {
     private final Pile pile;
     private final double yOffset; // spacing of the Panes
 
-    // private final GameController controller;
-    // private final int col;
+    // the state that belongs to the pileView as a whole
+    private int selectedIndex = -1;
 
     public PileView(Pile pile, double yOffset) {
         this.pile = pile;
@@ -26,6 +26,11 @@ public class PileView extends Pane {
 
     /**
      * The function answers -> how are the cards arranged inside this pileView
+     * 
+     * Handler click is in this function because handlers live on nodes, and if
+     * you rebuild a node, you must reattach handlers
+     * 
+     * Handler is a function given to javaFx to run later when something happens
      */
     public void redraw() {
         getChildren().clear(); // remove previous state
@@ -35,13 +40,25 @@ public class PileView extends Pane {
 
         for (int i = 0; i < cards.size(); i++) {
             Card card = cards.get(i);
+
+            // everytime redraw, you create new cardview
             CardView view = new CardView(card);
             view.setLayoutY(currentY);
 
+            // i'm selecting this cardView -> if X happens do Y -> if view clicked, do ...
             final int idx = i;
             view.setOnMouseClicked(e -> {
-                System.out.println("Clicked card at index: " + idx);
+                selectedIndex = idx;
+                redraw();
+                e.consume();
+                System.out.println("Selected Index: " + selectedIndex);
             });
+
+            if (i == selectedIndex) {
+                view.setStyle("-fx-border-color: gold;" + 
+                    "-fx-border-width: 3;"
+                );
+            }
 
             getChildren().add(view);
             currentY += yOffset;
