b9a5d36f1ce3cffc887ae0b646c57c653db77cda
LincolnKoskela <Lincolnkoskelal@gmail.com>
2025-12-29 12:00:19 -0600

    Tested CardSlot.

--- src/main/java/klondike/ui/GameApp.java
+++ src/main/java/klondike/ui/GameApp.java
@@ -1,9 +1,7 @@
 package klondike.ui;
 
 import javafx.application.*;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.*;
@@ -15,11 +13,6 @@ public class GameApp extends Application {
         StackPane root = new StackPane(); // root for top level node
         Scene scene = new Scene(root, 1200, 800, Color.DARKGREEN);
         root.setStyle("-fx-background-color: transparent;");
-
-        // test cardSlot 
-        root.getChildren().addAll(
-            new CardSlot("Waste")
-        );
 
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(scene);
