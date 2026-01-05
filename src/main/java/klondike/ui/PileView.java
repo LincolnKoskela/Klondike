b2ce4338efaae4e52b0133da8dee8ad7ea81a71b
LincolnKoskela <Lincolnkoskelal@gmail.com>
2026-01-05 10:36:15 -0600

    Updated README and pileview notes on redraw

--- src/main/java/klondike/ui/PileView.java
+++ src/main/java/klondike/ui/PileView.java
@@ -8,17 +8,15 @@ import java.util.*;
  * Abstract class PileView holds sync/updates. 
  * Subclasses will override for layout specs, the abstract function.
  */
-abstract class PileView extends Pane{
-    private final Pile pile;
-    private final List<CardView> cardViews = new ArrayList<>();
+abstract class PileView extends Pane {
+    protected final Pile pile; // engine model cards
+    protected final List<CardView> cardViews = new ArrayList<>(); // UI nodes/ view objects
+
+    protected abstract void layoutCards();
 
     public PileView(Pile pile) {
         this.pile = pile;
         redraw();
-    }
-
-    public Pile getPile() {
-        return pile;
     }
 
     /**
@@ -34,8 +32,15 @@ abstract class PileView extends Pane{
      * CASE B: model shrinks (card removed) -> Do I have more CardViews than cards
      * if yes, remove extras from the ends
      * 
-     * UPDATE + LAYOUT: make each view show the right card, the position it
+     * UPDATE -> cardViews are set to whatever the pile is
      * 
+     * Incremental synchronization keeps the view nodes in sync with the game model.
+     * On redraw(), the piles cards.size() is the TRUTH. If the UI has too many/few cardViews,
+     * it creates/removes only the difference, then updates each existing cardView to display the 
+     * correct card, then lays them out.
+     * This technique avoid destroying and then recreating the nodes everytime, leading to more 
+     * efficiency and preserving the nodes. This is enable animations and handling input easier
+     * and more clear to build.
      * 
      */
     public void redraw() {
@@ -53,7 +58,7 @@ abstract class PileView extends Pane{
             getChildren().remove(cv);
         }
 
-        // UPDATE + LAYOUT: refresh appearance and stack them
+        // UPDATE 
         for (int i = 0; i < cards.size(); i++) {
             CardView cv = cardViews.get(i);
             cv.setCard(cards.get(i));
@@ -67,5 +72,5 @@ abstract class PileView extends Pane{
         setPrefWidth(UiMetrics.CARD_W);
     }
 
-    protected abstract void layoutCards();
+    
 }
