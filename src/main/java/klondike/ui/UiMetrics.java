63ebe42b2d79ef79dc50cb41fc07ce4ebd1517e6
LincolnKoskela <Lincolnkoskelal@gmail.com>
2026-01-06 11:38:07 -0600

    Added more metrics for consistent boardlayout

--- src/main/java/klondike/ui/UiMetrics.java
+++ src/main/java/klondike/ui/UiMetrics.java
@@ -7,9 +7,17 @@ public class UiMetrics {
     public static final double CARD_H = 120;
     public static final double CARD_W = 80;
 
+    // origin of board
+    public static final double X0 = 60;
+    public static final double Y0 = 40;
+
     // Space between x and y 
     public static final double GAP_X = 20;
     public static final double GAP_Y = 30;
 
+    // others
     public static final double Y_OFFSET = 30;
+    public static final double STEP_X = CARD_W + GAP_X;
+    public static final double F_X = X0 + 3 * STEP_X;
+    public static final double T_Y = Y0 + (CARD_H + GAP_Y);
 }
